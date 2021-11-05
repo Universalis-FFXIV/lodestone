21e9e1c09250e9c9ae34e4ec42c277a55e5f7508
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-11-05 15:34:02 -0700

    Implement service

--- cmd/lodestone/main.go
+++ cmd/lodestone/main.go
@@ -1,10 +1,73 @@
 package main
 
 import (
+	"errors"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 	"github.com/karashiiro/bingode"
 	"github.com/xivapi/godestone/v2"
 )
 
 func main() {
-	_ = godestone.NewScraper(bingode.New(), godestone.EN)
+	s := godestone.NewScraper(bingode.New(), godestone.EN)
+
+	r := gin.Default()
+
+	// Character endpoint
+	r.GET("/character/:id", func(c *gin.Context) {
+		cIdStr := c.Param("id")
+
+		cId, err := strconv.ParseUint(cIdStr, 10, 32)
+		if err != nil {
+			c.AbortWithError(400, err)
+			return
+		}
+
+		character, err := s.FetchCharacter(uint32(cId))
+		if err != nil {
+			c.AbortWithError(404, err)
+			return
+		}
+
+		c.JSON(200, character)
+	})
+
+	// Character search endpoint
+	r.GET("/search/character/:worldName/:characterName", func(c *gin.Context) {
+		worldName := strings.ToLower(c.Param("worldName"))
+		if worldName == "" {
+			c.AbortWithError(400, errors.New("world name not provided"))
+		}
+
+		characterName := strings.ToLower(c.Param("characterName"))
+		if characterName == "" {
+			c.AbortWithError(400, errors.New("character name not provided"))
+		}
+
+		log.Println(worldName)
+		log.Println(characterName)
+		for res := range s.SearchCharacters(godestone.CharacterOptions{
+			Name:  characterName,
+			World: strings.ToUpper(string(worldName[0])) + worldName[1:], // World name must be captialized
+		}) {
+			log.Println(res)
+
+			if res.Error != nil {
+				c.AbortWithError(500, res.Error)
+				return
+			}
+
+			if strings.ToLower(res.Name) == characterName && strings.ToLower(res.World) == worldName {
+				c.JSON(200, res)
+				return
+			}
+		}
+
+		c.AbortWithError(404, errors.New("no character matching those parameters was found"))
+	})
+
+	r.Run(":3999")
 }
